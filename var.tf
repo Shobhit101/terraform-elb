3547689afeebfec7c30ed740cb174ed2fe2a3c28
Shobhit101 <54471884+Shobhit101@users.noreply.github.com>
2019-12-15 17:03:30 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,20 +1,20 @@
 variable "aws_vpc" {
     description = "VPC"
-    default = "vpc-c09fafab"
+    default = "vpc-a926dbc3"
 }
 
 variable "aws_subnet" {
     description = "aws subnet"
-    default = "subnet-acbfe9d1"
+    default = "subnet-e9021194"
 }
 
 variable "aws_subnet2" {
     description = "aws subnet2"
-    default = "subnet-1de7c250"
+    default = "subnet-7d937531"
 }
 
 variable "aws_key" {
     description = "aws key"
-    default = "DevOps-key"
+    default = "K8s_later"
 }
 
